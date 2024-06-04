7776e4cede5628b7f6cb998add7a35cfb02cc6a8
Xolmatov Foziljon <xolmatovfozil9@gmail.com>
2024-06-04 11:55:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/geminigetx/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.geminigetx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,3 @@
+const String SERVER_GEMINI = "generativelanguage.googleapis.com";
+
+const GEMINI_API_KEY = 'AAIzaSyDJMnD3qTayuduIgOrGFukrbf9AjORoc1o';
